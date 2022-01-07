62fedebfcdb163e9e8c17279a61ae01b42e1fef5
Dennis Box <dbox@fnal.gov>
2022-01-06 19:10:05 -0600

    added enstore.repo

--- ansible/templates/make-proxies.sh
+++ ansible/templates/make-proxies.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export X509_USER_CERT=/etc/grid-security/hostcert.pem
 export X509_USER_KEY=/etc/grid-security/hostkey.pem
-grid-proxy-init -valid 96:00 -rfc
+touch /tmp/x509up_u0
+grid-proxy-init -valid 96:00 -rfc || true
 /bin/cp /tmp/x509up_u0 /tmp/frontend_proxy
 /bin/cp /tmp/x509up_u0 /tmp/vo_proxy
 /bin/cp /tmp/x509up_u0 /tmp/grid_proxy
